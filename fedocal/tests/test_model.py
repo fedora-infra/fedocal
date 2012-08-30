2391040c5af75b499a697d6f055e75732cd43596
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-08-30 09:50:03 +0200

    Expand the test-suits to cover the Meeting object

--- fedocal/tests/test_model.py
+++ fedocal/tests/test_model.py
@@ -29,6 +29,8 @@
 __requires__ = ['SQLAlchemy >= 0.7']
 import pkg_resources
 
+from datetime import date
+from datetime import time
 import unittest
 import sys
 import os
@@ -102,6 +104,49 @@ class Modeltests(unittest.TestCase):
             'fi-apprentice@lists.fedoraproject.org,'\
             'ambassadors@lists.fedoraproject.org')
         self.assertTrue(obj.reminder_text, 'This is your friendly reminder')
+
+    def test_init_meeting(self):
+        """ Test the Meeting init function. """
+        self.test_init_calendar()
+        obj = model.Meeting(
+            'Fedora-fr-test-meeting',
+            'pingou, shaiton',
+            date(2012, 9, 3),
+            date(2012, 9, 3),
+            time(19,00),
+            time(20,00),
+            'test_calendar',
+            None)
+        obj.save(self.session)
+        self.session.commit()
+        self.assertNotEqual(obj, None)
+
+    def test_init_meeting(self):
+        """ Test the Meeting init function. """
+        self.test_init_calendar()
+        obj = model.Meeting(
+            'Fedora-fr-test-meeting',
+            'pingou, shaiton',
+            date.today(),
+            date.today(),
+            time(19,00),
+            time(20,00),
+            'test_calendar',
+            None)
+        obj.save(self.session)
+        self.session.commit()
+        self.assertNotEqual(obj, None)
+
+    def test_get_meeting(self):
+        """ Test the query of a meeting by its identifier. """
+        self.test_init_meeting()
+        obj = model.Meeting.by_id(self.session, 1)
+        self.assertNotEqual(obj, None)
+        self.assertTrue(obj.meeting_name, 'Fedora-fr-test-meeting')
+        self.assertTrue(obj.meeting_manager, 'pingou, shaiton')
+        self.assertTrue(obj.calendar.calendar_name, 'test_calendar')
+        self.assertTrue(obj.calendar.calendar_description, 'This is a test calendar')
+        self.assertEqual(obj.reminder, None)
 
 
 if __name__ == '__main__':

b9d3515d7a19c5fdf103fb21e9186bf63ae754f2
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-11-06 09:02:48 +0100

    Remove TODO, now done

--- fedocal/fedocallib/fedora_calendar.py
+++ fedocal/fedocallib/fedora_calendar.py
@@ -37,8 +37,6 @@ class FedocalCalendar(HTMLCalendar):
         """
         Return a day as a table cell.
         """
-        ## TODO: Fix the links so that they point to something that
-        # makes sense
         cur_date = date.today()
         if day == 0:
             return '<td class="noday">&nbsp;</td>'  # day outside month

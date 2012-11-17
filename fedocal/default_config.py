6f7b5aa337fcbb2868790cae1a75999545d7359b
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-11-17 20:06:34 +0100

    pep8 and pylint run on default_config

--- fedocal/default_config.py
+++ fedocal/default_config.py
@@ -25,7 +25,8 @@
 
 
 # url to the database server:
-DB_URL='sqlite:////tmp/fedocal_dev.sqlite'
+DB_URL = 'sqlite:////tmp/fedocal_dev.sqlite'
+
 
 # The FAS group in which the admin of fedocal are
-ADMIN_GROUP='fedocal_admin'
+ADMIN_GROUP = 'fedocal_admin'

5e8e108d1e6230ce9b9b74e0b2d1118168ff5c8b
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-11-25 15:12:29 +0100

    Add a delete method to the Calendar object

--- fedocal/fedocallib/model.py
+++ fedocal/fedocallib/model.py
@@ -93,6 +93,10 @@ class Calendar(BASE):
         """ Save the object into the database. """
         session.add(self)
 
+    def delete(self, session):
+        """ Remove the object into the database. """
+        session.delete(self)
+
     @classmethod
     def by_id(cls, session, identifier):
         """ Retrieve a Calendar object from the database based on its

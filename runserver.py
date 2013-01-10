c4425a28d182e3eb46ff7a7dbfa5ae1cb3437a12
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-01-10 21:37:21 +0100

    Add import pkg_resources to be able to run fedocal on EL6

--- runserver.py
+++ runserver.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+import pkg_resources
+
 from fedocal import APP
 APP.debug=True
 APP.run()

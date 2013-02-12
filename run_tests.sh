5e913c404ec8ffd253436a4f98a9de2d9ad1cb09
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-02-12 11:02:57 +0100

    Add --cover-erase to make sure we have up to date stats on the coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FEDOCAL_CONFIG=tests/fedocal_test.cfg PYTHONPATH=fedocal nosetests --with-coverage --cover-package=fedocal $*
+FEDOCAL_CONFIG=tests/fedocal_test.cfg PYTHONPATH=fedocal nosetests --with-coverage --cover-erase --cover-package=fedocal $*

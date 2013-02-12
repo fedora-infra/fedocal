e5c7e6083df966ddab45dc2abc837da18ca04b09
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-02-12 15:13:18 +0100

    Add first version of installer
    This commits adds the first version of the setup.py script and its
    corresponding MANIFEST.in file providing the list of files and folders
    that should be included in the sources when making a release.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Setup script
+"""
+
+from setuptools import setup, find_packages
+from fedocal import __version__
+
+setup(
+    name = 'fedocal',
+    description = 'fedocal is a web based calendar application for Fedora.',
+    version = __version__,
+    author = 'Pierre-Yves Chibon',
+    author_email = 'pingou@pingoured.fr',
+    maintainer = 'Pierre-Yves Chibon',
+    maintainer_email = 'pingou@pingoured.fr',
+    license = 'GPLv3+',
+    download_url = 'https://fedorahosted.org/releases/f/e/fedocal/',
+    url = 'https://fedorahosted.org/fedocal/',
+    packages=['fedocal'],
+    include_package_data = True,
+    exclude_package_data={'': ['tests/*']},
+    install_requires=['Flask']
+    )

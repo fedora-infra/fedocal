141332470f29b4b250377b6feddee46557406c00
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-05-27 18:43:42 +0200

    Add the alembic migration for the calendar_admin_group field in the calendar table

---
+++ fedocal/alembic/versions/43bc10530adb_add_calendar_admin.py
@@ -0,0 +1,23 @@
+"""Add calendar admin
+
+Revision ID: 43bc10530adb
+Revises: 2e5d25a095df
+Create Date: 2013-05-27 18:17:12.891156
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '43bc10530adb'
+down_revision = '2e5d25a095df'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('calendars', sa.Column('calendar_admin_group',
+        sa.String(100), nullable=True))
+
+
+def downgrade():
+    op.drop_column('calendars', 'calendar_admin_group')

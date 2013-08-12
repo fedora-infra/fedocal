c1d24ab6ba96547abccf3dd2eb1764f783d53abf
Pierre-Yves Chibon <pingou@pingoured.fr>
2013-08-12 22:00:21 +0200

    Bump version number to prepare for the release

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ copyright = u'2012, Pierre-Yves Chibon <pingou@pingoured.fr>'
 #
 # The short X.Y version.
 #version = __version__
-version = '0.1.2'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags.
 #release = '1'
 
--- fedocal/__init__.py
+++ fedocal/__init__.py
@@ -27,7 +27,7 @@
 __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 import datetime
 import os

136cbd20f6ef2c0189fe2f3c0e539aafd5c70ec4
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-01-16 13:43:58 +0100

    Add tests for add_meeting, edit_meeting, auth_logout, my_meetings and more for auth_login

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -31,11 +31,14 @@ __requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
 import pkg_resources
 
 import flask
+import logging
 import unittest
 import sys
 import os
 
 from datetime import date
+from datetime import time
+from datetime import timedelta
 
 sys.path.insert(0, os.path.join(os.path.dirname(
     os.path.abspath(__file__)), '..'))
@@ -43,7 +46,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(
 import fedocal
 import fedocal.fedocallib as fedocallib
 import fedocal.fedocallib.model as model
-from tests import Modeltests, FakeUser
+from tests import Modeltests, FakeUser, flask10_only, user_set, TODAY
 
 
 # pylint: disable=E1103
@@ -63,6 +66,9 @@ class Flasktests(Modeltests):
         super(Flasktests, self).setUp()
 
         fedocal.APP.config['TESTING'] = True
+        fedocal.APP.debug = True
+        fedocal.APP.logger.handlers = []
+        fedocal.APP.logger.setLevel(logging.CRITICAL)
         fedocal.SESSION = self.session
         self.app = fedocal.APP.test_client()
 
@@ -415,6 +421,16 @@ class Flasktests(Modeltests):
             output = self.app.get('/login/?next=http://localhost/')
             self.assertEqual(output.status_code, 200)
 
+    @flask10_only
+    def test_auth_login_logedin(self):
+        """ Test the auth_login function. """
+        self.__setup_db()
+        user = FakeUser([], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/login/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<title>Home - Fedocal</title>' in output.data)
+
     def test_locations(self):
         """ Test the locations function. """
         self.__setup_db()
@@ -465,6 +481,412 @@ class Flasktests(Modeltests):
         self.assertTrue(
             'class="errors">No location named foobar could not be found</'
             in output.data)
+
+    @flask10_only
+    def test_add_calendar(self):
+        """ Test the add_calendar function. """
+        user = None
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/calendar/add/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>OpenID transaction in progress</title>'
+                in output.data)
+
+        user = FakeUser(['test'])
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/calendar/add/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '"errors">You are not a fedocal admin, you are not allowed '
+                'to add calendars.</' in output.data)
+
+        user = FakeUser(fedocal.APP.config['ADMIN_GROUP'])
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/calendar/add/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Add calendar - Fedocal</title>' in output.data)
+            self.assertTrue(
+                'for="calendar_name">Calendar <span class="error">*</span>'
+                in output.data)
+            self.assertTrue(
+                'contact">Contact email <span class="error">*</span>'
+                in output.data)
+
+            csrf_token = output.data.split(
+                'name="csrf_token" type="hidden" value="')[1].split('">')[0]
+
+            # calendar should have a name
+            data = {
+                'calendar_contact': 'election1',
+                'calendar_status': 'Enabled',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<td>This field is required.</td>' in output.data)
+
+            # Works
+            data = {
+                'calendar_name': 'election1',
+                'calendar_contact': 'election1',
+                'calendar_status': 'Enabled',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="message">Calendar added</li>' in output.data)
+
+            # This calendar already exists
+            data = {
+                'calendar_name': 'election1',
+                'calendar_contact': 'election1',
+                'calendar_status': 'Enabled',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '="errors">Could not add this calendar to the database</'
+                in output.data)
+
+    @flask10_only
+    def test_auth_logout(self):
+        """ Test the auth_logout function. """
+        user = FakeUser(fedocal.APP.config['ADMIN_GROUP'])
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/logout/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<title>Home - Fedocal</title>' in output.data)
+            self.assertTrue(
+                '<li class="message">You have been logged out</li>'
+                in output.data)
+
+        user = None
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/logout/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue('<title>Home - Fedocal</title>' in output.data)
+            self.assertFalse(
+                '<li class="message">You have been logged out</li>'
+                in output.data)
+
+    @flask10_only
+    def test_my_meetings(self):
+        """ Test the my_meetings function. """
+        self.__setup_db()
+        user = FakeUser([], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/mine/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '"errors">You must be in one more group than the CLA</li>' in output.data)
+
+        user = FakeUser(['packager'], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/mine/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>My meeting - Fedocal</title>' in output.data)
+            self.assertTrue(
+                '<td> Full-day meeting </td>' in output.data)
+            self.assertTrue(
+                '<td> test-meeting2 </td>' in output.data)
+            self.assertTrue(
+                '<td> Test meeting with reminder </td>' in output.data)
+
+    @flask10_only
+    def test_add_meeting(self):
+        """ Test the add_meeting function. """
+        self.__setup_db()
+        user = FakeUser(['gitr2spec'], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/calendar_test/add/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '"errors">No calendar named calendar_test could not be found</'
+                in output.data)
+
+            output = self.app.get('/test_calendar/add/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="errors">You are not one of the editors of this '
+                'calendar, or one of its admins, you are not allowed to add'
+                ' new meetings.</li>' in output.data)
+            self.assertTrue(
+                '<title>test_calendar - Fedocal</title>' in output.data)
+
+        user = FakeUser(['fi-apprentice'], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/test_calendar/add/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Add meeting - Fedocal</title>' in output.data)
+            self.assertTrue(
+                'meeting_name">Meeting name <span class="error">*</span></l'
+                in output.data)
+            self.assertTrue(
+                'for="meeting_date">Date <span class="error">*</span></label'
+                in output.data)
+
+            csrf_token = output.data.split(
+                'name="csrf_token" type="hidden" value="')[1].split('">')[0]
+
+            # Meeting should have a name
+            data = {
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/test_calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<td>This field is required.</td>' in output.data)
+
+            # End date earlier than the start date
+            data = {
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_date_end': TODAY - timedelta(days=1),
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/test_calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="warnings">The start date of your meeting is '
+                'later than the stop date.</li>' in output.data)
+            self.assertTrue(
+                '<title>Add meeting - Fedocal</title>' in output.data)
+
+            # Works
+            data = {
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/test_calendar/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="message">Meeting added</li>' in output.data)
+            self.assertTrue(
+                'href="/meeting/15/">' in output.data)
+
+            # Calendar disabled
+            data = {
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/test_calendar_disabled/add/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>test_calendar_disabled - Fedocal</title>'
+                in output.data)
+            self.assertTrue(
+                '<li class="errors">This calendar is &#34;Disabled&#34;, '
+                'you are not allowed to add meetings anymore.</li>'
+                in output.data)
+
+    @flask10_only
+    def test_edit_meeting(self):
+        """ Test the edit_meeting function. """
+        self.__setup_db()
+        user = FakeUser(['gitr2spec'], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/meeting/edit/50/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                'class="errors">The meeting #50 could not be found</li>'
+                in output.data)
+            self.assertTrue('<title>Home - Fedocal</title>' in output.data)
+
+            output = self.app.get('/meeting/edit/1/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="errors">You are not one of the editors of this '
+                'calendar, or one of its admins, you are not allowed to edit'
+                ' meetings.</li>' in output.data)
+            self.assertTrue(
+                '<title>test_calendar - Fedocal</title>' in output.data)
+
+        user = FakeUser(['fi-apprentice'], username='kevin')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/meeting/edit/1/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="errors">You are not one of the manager of this '
+                'meeting, or an admin, you are not allowed to edit it.</li>'
+                in output.data)
+            self.assertTrue(
+                '<title>Meeting Fedora-fr-test-meeting - Fedocal</title>'
+                in output.data)
+
+        user = FakeUser(['fi-apprentice'], username='pingou')
+        with user_set(fedocal.APP, user):
+            output = self.app.get('/meeting/edit/1/')
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Edit meeting - Fedocal</title>' in output.data)
+            self.assertTrue(
+                'meeting_name">Meeting name <span class="error">*</span></l'
+                in output.data)
+            self.assertTrue(
+                'for="meeting_date">Date <span class="error">*</span></label'
+                in output.data)
+
+            csrf_token = output.data.split(
+                'name="csrf_token" type="hidden" value="')[1].split('">')[0]
+
+            # Meeting should have a name
+            data = {
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/meeting/edit/1/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<td>This field is required.</td>' in output.data)
+
+            # No calendar provided
+            data = {
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_date_end': TODAY - timedelta(days=1),
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/meeting/edit/1/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<td>Not a valid choice</td>' in output.data)
+            self.assertTrue(
+                '<title>Edit meeting - Fedocal</title>' in output.data)
+
+            # End date earlier than the start date
+            data = {
+                'calendar_name': 'test_calendar',
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_date_end': TODAY - timedelta(days=1),
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/meeting/edit/1/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="warnings">The start date of your meeting is '
+                'later than the stop date.</li>' in output.data)
+            self.assertTrue(
+                '<title>Edit meeting - Fedocal</title>' in output.data)
+
+            # Works
+            data = {
+                'calendar_name': 'test_calendar',
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/meeting/edit/1/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<li class="message">Meeting updated</li>' in output.data)
+            self.assertTrue(
+                '<title>Meeting guess what? - Fedocal</title>' in output.data)
+
+            # Calendar disabled
+            data = {
+                'calendar_name': 'test_calendar_disabled',
+                'meeting_name': 'guess what?',
+                'meeting_date': TODAY,
+                'meeting_time_start': time(13, 0),
+                'meeting_time_stop': time(14, 0),
+                'meeting_timezone': 'Europe/Paris',
+                'frequency': '',
+                'csrf_token': csrf_token,
+            }
+
+            output = self.app.post('/meeting/edit/1/', data=data,
+                                   follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>test_calendar_disabled - Fedocal</title>'
+                in output.data)
+            self.assertTrue(
+                '<li class="errors">This calendar is &#34;Disabled&#34;, '
+                'you are not allowed to add meetings to it anymore.</li>'
+                in output.data)
+
+        user = FakeUser(['packager'], username='pingou')
+        with user_set(fedocal.APP, user):
+            # Recursive meeting
+            output = self.app.get('/meeting/edit/12/', follow_redirects=True)
+            self.assertEqual(output.status_code, 200)
+            self.assertTrue(
+                '<title>Edit meeting - Fedocal</title>' in output.data)
+            self.assertTrue(
+                '<h2>Edit meeting Another past test meeting</h2>'
+                in output.data)
+            self.assertTrue(
+                'meeting_name">Meeting name <span class="error">*</span></l'
+                in output.data)
+            self.assertTrue(
+                'for="meeting_date">Date <span class="error">*</span></label'
+                in output.data)
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(Flasktests)

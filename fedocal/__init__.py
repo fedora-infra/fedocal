19118ba07e830ddccd57b0c59f567e6ded3816b9
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-06-17 12:45:07 +0200

    Add the possibility to set the end date via the ?delta argument
    This way we can list all the meeting over 2 weeks by just setting the
    start date in the url and use ?delta=15

--- fedocal/__init__.py
+++ fedocal/__init__.py
@@ -401,6 +401,11 @@ def calendar_list(calendar_name, year=None, month=None, day=None):
     :arg day: the day of the date one would like to consult.
     """
     subject = flask.request.args.get('subject', None)
+    delta = flask.request.args.get('delta', 1)
+    try:
+        delta = int(delta)
+    except ValueError:
+        delta = 1
 
     today = datetime.date.today()
     inyear = year
@@ -422,7 +427,7 @@ def calendar_list(calendar_name, year=None, month=None, day=None):
             + relativedelta(months=+1) \
             - datetime.timedelta(days=1)
     else:
-        end_date = start_date + relativedelta(days=+1)
+        end_date = start_date + relativedelta(days=+delta)
 
     calendarobj = Calendar.by_id(SESSION, calendar_name)
     if not calendarobj:

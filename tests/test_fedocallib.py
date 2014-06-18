f22f1377f2ba8c5cbfbbe82df94cc2a770079798
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-06-18 11:29:31 +0200

    Increase coverage of the delete_recursive_meeting by testing deleting a recursive meeting in the future

--- tests/test_fedocallib.py
+++ tests/test_fedocallib.py
@@ -534,6 +534,30 @@ class Fedocallibtests(Modeltests):
                 meeting.recursion_ends,
                 TODAY + timedelta(days=13))
 
+    def test_delete_recursive_meeting_future(self):
+        """ Test the delete_recursive_meeting function. """
+        self.__setup_meeting()
+        meeting = model.Meeting.by_id(self.session, 8)
+        self.assertNotEqual(meeting, None)
+        self.assertEqual(
+            meeting.meeting_name, 'Another test meeting2')
+        self.assertEqual(
+            meeting.recursion_ends, TODAY + timedelta(days=90))
+
+        del_date = TODAY + timedelta(days=42)
+        fedocallib.delete_recursive_meeting(
+            self.session, meeting, del_date=del_date)
+
+        meeting = model.Meeting.by_id(self.session, 8)
+        self.assertNotEqual(meeting, None)
+        self.assertEqual(meeting.meeting_name, 'Another test meeting2')
+        # End date is one day before the next occurence of the recursion
+        # Meeting 8 starts TODAY and recurs every 14 days.
+        # The meeting now stops before its next occurence.
+        self.assertEqual(
+            meeting.recursion_ends,
+            TODAY + timedelta(days=41))
+
     # pylint: disable=C0103
     def test_delete_recursive_meeting_past(self):
         """ Test the delete_recursive_meeting for past end_datefunction.

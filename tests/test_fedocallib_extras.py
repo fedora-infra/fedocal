2ca0c1bc49eaea10c0507805f3c5046a9fe1f7de
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-10-06 17:22:37 +0200

    Add test when deleting a recursive meeting at a date before it starts occurring

--- tests/test_fedocallib_extras.py
+++ tests/test_fedocallib_extras.py
@@ -162,6 +162,35 @@ class FedocallibExtratests(Modeltests):
             ]
         )
 
+        # Delete meeting before the start of the recursion
+        # This will delete the first occurrence of the meeting as it is the
+        # first one after the date specified.
+        meeting = model.Meeting.by_id(self.session, 1)
+        fedocallib.delete_recursive_meeting(
+            self.session,
+            meeting=meeting,
+            del_date=datetime(2014, 8, 18).date(),
+            all_meetings=False)
+
+        # After deletion
+        meetings = fedocallib.get_by_date(
+            self.session, calendarobj,
+            datetime(2014, 8, 1).date(),
+            datetime(2015, 10, 27).date()
+        )
+        self.assertEqual(len(meetings), 7)
+        self.assertEqual(len(ids), 1)
+        self.assertEqual(list(ids)[0], 1)
+        dates = [str(mtg.meeting_date) for mtg in meetings]
+        self.assertEqual(
+            dates,
+            [
+                '2014-09-08', '2014-09-15', '2014-09-22',
+                '2014-09-29', '2014-10-06', '2014-10-13',
+                '2014-10-27',
+            ]
+        )
+
 
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(FedocallibExtratests)

41313422c2531fd92f67273c0665e68f735259a3
Pierre-Yves Chibon <pingou@pingoured.fr>
2016-06-29 11:49:47 +0200

    Rework the runserver script to allow specifying the host/port and config file

--- runserver.py
+++ runserver.py
@@ -1,9 +1,49 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
-## These two lines are needed to run on EL6
-__requires__ = ['SQLAlchemy >= 0.7', 'jinja2 >= 2.4']
+# These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
 import pkg_resources
 
+import argparse
+import sys
+import os
+
+
+parser = argparse.ArgumentParser(
+    description='Run the fedocal app')
+parser.add_argument(
+    '--config', '-c', dest='config',
+    help='Configuration file to use for fedocal.')
+parser.add_argument(
+    '--debug', dest='debug', action='store_true',
+    default=False,
+    help='Expand the level of data returned.')
+parser.add_argument(
+    '--profile', dest='profile', action='store_true',
+    default=False,
+    help='Profile fedocal.')
+parser.add_argument(
+    '--port', '-p', default=5000,
+    help='Port for the fedocal to run on.')
+parser.add_argument(
+    '--host', default="127.0.0.1",
+    help='Hostname to listen on. When set to 0.0.0.0 the server is available externally. Defaults to 127.0.0.1 making the it only visable on localhost')
+
+args = parser.parse_args()
+
+if args.config:
+    config = args.config
+    if not config.startswith('/'):
+        here = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+        config = os.path.join(here, config)
+    os.environ['FEDOCAL_CONFIG'] = config
+
 from fedocal import APP
+
+if args.profile:
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    APP.config['PROFILE'] = True
+    APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
+
 APP.debug = True
-APP.run()
+APP.run(host=args.host, port=int(args.port))
